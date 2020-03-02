e254f152934e6611a4771f58fe0638d6780eb405
Leonardo <37965130+Luker64@users.noreply.github.com>
2020-03-02 17:44:18 -0300

    Refactoring voting

--- voting.js
+++ voting.js
@@ -1,52 +1,67 @@
 const utils = require('./utils.js')
 
+function processVote(msgOwnerIndex, playerVotedIndex) {
+    if (playerVotedIndex != -1) {
+        if (game.players[playerVotedIndex].votes) {
+            game.players[playerVotedIndex].votes += 1;
+        } else {
+            game.players[playerVotedIndex].votes = 1;
+        }
+        game.players[msgOwnerIndex].voted = playerVotedIndex
+        game.votes += 1
+    } else {
+        msg.reply("Esta pessoa não está jogando")
+    }
+}
+
+function finishVoteSession(msg) {
+    msg.channel.send("Acabou a rodada dos votos")
+    var playerVoted;
+    var playerVotedIndex;
+    game.players.forEach((player, index) => {
+        if(!player.votes){
+            player.votes = 0;
+        }
+        if(!playerVoted || player.votes > playerVoted.votes){
+            playerVoted = player;
+            playerVotedIndex = index;
+        }
+    })
+    announceWhoIsTheVoted(msg, playerVoted, playerVotedIndex)
+}
+
+function announceWhoIsTheVoted(msg, playerVoted, playerVotedIndex){
+    msg.channel.send(`Jogador mais votado foi: <@${playerVoted.id}>`)
+    game.players.splice(playerVotedIndex, 1)
+    if (playerVoted.roles == "Spy") {
+        msg.channel.send('O SPY foi descoberto, escolha o lugar com ```spy location <lugar>```')
+    } else {
+        msg.channel.send("O Spy continua no jogo")
+    }
+}
+
 module.exports = {
     voteInfo: (msg, bot) => {
         var embed = utils.embed({
-            msg, title:'Votos', thumb: 'https://cdn.discordapp.com/attachments/368818652109078532/683790044016017549/topsecret.png'
+            msg, title: 'Votos', thumb: 'https://cdn.discordapp.com/attachments/368818652109078532/683790044016017549/topsecret.png'
         }, bot)
         embed.setDescription('Para realizar o voto, utilize: ```spy vote @<Nome de usuário>```')
-        msg.reply(embed);
+        msg.channel.send(embed);
     },
-    voting: (msg, bot) =>{ 
-        ownerIndex = utils.searchPlayerIndex(msg.author.id)
-        if(game.players[ownerIndex].voted){
+    voting: (msg, bot) => {
+        msgOwnerIndex = utils.searchPlayerIndex(msg.author.id)
+        if (game.players[msgOwnerIndex].voted) {
             msg.reply("Você só pode votar uma vez!")
             return;
         }
-        var playerIndex = utils.searchPlayerIndex(msg.mentions.users.first().id);
-        if(playerIndex != -1){
-            if(game.players[playerIndex].votes){
-                game.players[playerIndex].votes += 1;
-            }else{
-                game.players[playerIndex].votes = 1;
+        try{
+            var playerVotedIndex = utils.searchPlayerIndex(msg.mentions.users.first().id);
+            processVote(msgOwnerIndex, playerVotedIndex)
+            if (game.votes == game.players.length) {
+                finishVoteSession(msg)
             }
-            game.players[ownerIndex].voted = playerIndex
-            game.votes += 1 
-        }
-        if(game.votes == game.players.length){
-            msg.reply("acabou os votos")
-            var playerVoted = game.players[0];
-            var playerVotedIndex = 0;
-            if(!playerVoted.votes){
-                playerVoted.votes = 0;
-            }
-            for(player in game.players){
-                if(!game.players[player].votes){
-                    game.players[player].votes = 0;
-                    playerVotedIndex = player;
-                }
-                if(game.players[player].votes > playerVoted.votes){
-                    playerVoted = game.players[player];
-                }
-            }
-            msg.reply(`Jogador mais votado foi: <@${playerVoted.id}>`)
-            game.players.splice(playerVotedIndex, 1)
-            if(playerVoted.roles == "Spy"){
-                msg.reply('O SPY foi descoberto, escolha o lugar com ```spy location <lugar>```')
-            }else{
-                msg.reply("O Spy continua no jogo")
-            }
+        }catch{
+            msg.channel.send("Vota certo MEU!")
         }
     }
 }
