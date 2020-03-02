8c1e078ff0f6a41785881bade3a62ba63d736474
cavebran <guilherme.cavequia@gmail.com>
2020-03-02 20:48:16 -0300

    fix of locationsInfo.js and added new locations in locations.json

--- locationsInfo.js
+++ locationsInfo.js
@@ -4,9 +4,10 @@ const utils = require('./utils')
 module.exports = {
     showLocations: (msg, bot) => {
         var embed = utils.embed({msg, title: 'Todos Locais', thumb:'https://images.emojiterra.com/twitter/v12/512px/2753.png'}, bot);
-        for(location in locations.locations){
-            embed.addField(parseInt(location)+1 + ':', locations.locations[location].name)
+
+        for (let i = 0; i < locations.locations.length; i+=2) {
+            embed.addField(locations.locations[i].name, locations.locations[1+i].name, true)
         }
-        msg.reply(embed)
+        msg.channel.send(embed)
     }
 }
